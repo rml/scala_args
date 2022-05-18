e935563c729814813bd2ebf1ec8385756fc64ac8
rml <rml@users.noreply.github.com>
2022-05-18 13:47:50 +0200

    Changed enum-name (Scala 3 keyword)

--- src/main/scala/rml/args/conversions/map/ToEnum.scala
+++ src/main/scala/rml/args/conversions/map/ToEnum.scala
@@ -7,53 +7,53 @@ import rml.args.exceptions.IllegalArgException
 
 trait ToEnum[E <: Enumeration] {
 
-  val enum: E
+  val anEnum: E
 
   def mapToType(value: String): E#Value = try {
-    enum.withName(value)
+    anEnum.withName(value)
   } catch {
     case nsee: NoSuchElementException =>
       throw new IllegalArgException(
         "",
         value,
-        (0 until enum.maxId).toList.map(enum(_).toString)
+        (0 until anEnum.maxId).toList.map(anEnum(_).toString)
       )
   }
 
   lazy val getRestriction: FixRestriction = FixRestriction(
-    enum.values.map(_.toString)
+    anEnum.values.map(_.toString)
   )
 
-  lazy val baseType: String = "Enumeration " + enum.getClass.getName
+  lazy val baseType: String = "Enumeration " + anEnum.getClass.getName
 
-  override def toString: String = enum.values.mkString("[", ", ", "]")
+  override def toString: String = anEnum.values.mkString("[", ", ", "]")
 }
 
 object AnEnum {
   def apply[E <: Enumeration](key: String, e: E): InputArg[E#Value] =
-    InputArg(key, new SingleArg[E#Value] with ToEnum[E] { val enum: E = e })
+    InputArg(key, new SingleArg[E#Value] with ToEnum[E] { val anEnum: E = e })
 }
 
 object JEnum {
   def apply[E <: Enumeration](key: String, e: E): InputArg[E#Value] =
-    InputArg(key, new JoinArg[E#Value] with ToEnum[E] { val enum: E = e })
+    InputArg(key, new JoinArg[E#Value] with ToEnum[E] { val anEnum: E = e })
 }
 
 object Enums {
   def apply[E <: Enumeration](key: String, e: E): InputArg[List[E#Value]] =
-    InputArg(key, new ListArg[E#Value] with ToEnum[E] { val enum: E = e })
+    InputArg(key, new ListArg[E#Value] with ToEnum[E] { val anEnum: E = e })
 }
 
 object Enums0 {
   def apply[E <: Enumeration](key: String, e: E): InputArg[List[E#Value]] =
-    InputArg(key, new ListArg0[E#Value] with ToEnum[E] { val enum: E = e })
+    InputArg(key, new ListArg0[E#Value] with ToEnum[E] { val anEnum: E = e })
 }
 
 object PEnum {
   def apply[E <: Enumeration](pos: Int, e: E): InputArg[E#Value] = InputArg(
     "-",
     new ToEnum[E] with PositionalArg[E#Value] {
-      val enum: E = e; val position: Int = pos
+      val anEnum: E = e; val position: Int = pos
     }
   )
 }
